2520d4e6d90031c38986c9098bfe907d4791f9a1
ssc-317 <suneetha.sca@gmail.com>
2021-02-18 10:24:50 +0000

    added missing index command

--- bwa_alignment_sort_index_merge.sh
+++ bwa_alignment_sort_index_merge.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+bwa index Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa
+
 for n in {1..2}
 do
 	bwa mem -R '@RG\tID:'"$n"'\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
@@ -14,4 +16,9 @@ done
 picard MergeSamFiles INPUT=lane1_sorted.bam INPUT=lane2_sorted.bam OUTPUT=library.bam
 
 samtools index library.bam
+
+#for n in {1..2}
+#do
+#samtools view -C -T Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa lane${n}_sorted.bam > lane${n}_sorted.cram
+#done
 
